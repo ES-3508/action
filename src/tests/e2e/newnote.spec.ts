a0ca81882e2beb9577d48e130e68643ac5bef94a
Eranda Sanjaya <76166067+ES-3508@users.noreply.github.com>
2024-08-29 09:12:29 +0530

    a

--- src/tests/e2e/newnote.spec.ts
+++ src/tests/e2e/newnote.spec.ts
@@ -1,12 +1,15 @@
 import { test, expect } from '@playwright/test';
 
 test('test', async ({ page }) => {
+  console.log('Starting E2E Test');
 
   // Go to http://localhost:3000/
   await page.goto('http://localhost:3000/');
+  console.log('Navigated to http://localhost:3000/');
 
   // Click text=New Note
   await page.locator('text=New Note').click();
+  console.log('Clicked on New Note');
   await expect(page).toHaveURL('http://localhost:3000/new');
 
   // Click [placeholder="Enter Note Title"]
@@ -24,9 +27,10 @@ test('test', async ({ page }) => {
   // Click text=Save
   await page.locator('text=Save').click();
   await expect(page).toHaveURL('http://localhost:3000/');
+  console.log('Saved the note');
 
   // Click text=Delete
   await page.locator('text=Delete').click();
   await expect(page).toHaveURL('http://localhost:3000/');
-
+  console.log('Deleted the note');
 });
